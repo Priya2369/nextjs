48e5670eb2297159fd9cb212f28a53145e7693e6
priyambada <spriyambada2369@gmail.com>
2021-03-12 17:46:35 +0530

    3rd commit

--- components/footer.js
+++ components/footer.js
@@ -1,29 +1,29 @@
 const Footer = () =>{
     return(
-        <footer class="footer">
-		<div class="inner-footer">
-			<div class="quick-links">
+        <footer className="footer">
+		<div className="inner-footer">
+			<div className="quick-links">
 				<ul>
-				<li class="quick-items"><a href="">Jobs</a></li>
-				<li class="quick-items"><a href="">Companies</a></li>
-				<li class="quick-items"><a href="">About</a></li>
-				<li class="quick-items"><a href="">Contact</a></li>
+				<li className="quick-items"><a href="">Jobs</a></li>
+				<li className="quick-items"><a href="">Companies</a></li>
+				<li className="quick-items"><a href="">About</a></li>
+				<li className="quick-items"><a href="">Contact</a></li>
 				</ul>
 			</div>
-			<div class="social-links">
+			<div className="social-links">
 				<ul>
-				<li className="social-items"><a href="#">h<i class="fab fa-facebook-square"></i></a></li>
-				<li class="social-items"><a href="#">h<i class="fab fa-twitter-square"></i></a></li>
-				<li class="social-items"><a href="#">h<i class="fab fa-instagram"></i></a></li>
-				<li class="social-items"><a href="#">h<i class="fab fa-youtube"></i></a></li>
+				<li className="social-items"><a href="#">h<i className="fab fa-facebook-square"></i></a></li>
+				<li className="social-items"><a href="#">h<i className="fab fa-twitter-square"></i></a></li>
+				<li className="social-items"><a href="#">h<i className="fab fa-instagram"></i></a></li>
+				<li className="social-items"><a href="#">h<i className="fab fa-youtube"></i></a></li>
 			   </ul>	
 			</div>
 			<div className="hori-line"></div>
-			<div class="footer-heading footer-3">
+			<div className="footer-heading footer-3">
 				<a href="index.html"><img src="/logo-g.png"/></a>
 			</div>
 		</div>
-			<div class="outer-footer"><b>
+			<div className="outer-footer"><b>
 				© Copyright &copy;2021.All Rights Reserved.   Privacy-Terms
 				</b>
 			</div>
--- components/navbar.js
+++ components/navbar.js
@@ -5,25 +5,25 @@ const Navbar = () => {
     return (
         <>
 
-            <div class="header">
-                <div class="header-logo">
+            <div className="header">
+                <div className="header-logo">
                     <a>Email:mosahay.adm@gmail.com</a>
 
 
                 </div>
-                <div class="float-right-header">
-                    <a href="#">h<i class="fab fa-facebook-square"></i></a>
-                    <a href="#">h<i class="fab fa-twitter-square"></i></a>
-                    <a href="#">h<i class="fab fa-instagram"></i></a>
-                    <a href="#">h<i class="fab fa-youtube"></i></a>
+                <div className="float-right-header">
+                    <a href="#">h<i className="fab fa-facebook-square"></i></a>
+                    <a href="#">h<i className="fab fa-twitter-square"></i></a>
+                    <a href="#">h<i className="fab fa-instagram"></i></a>
+                    <a href="#">h<i className="fab fa-youtube"></i></a>
 
                 </div>
             </div>
-            <div class="navbar">
-                <div class="navbar-logo">
+            <div className="navbar">
+                <div className="navbar-logo">
                     <a href="index.html"><img src="logo-g.png" /></a>
                 </div>
-                <div class="float-navbar">
+                <div className="float-navbar">
                     <Link href="/"><a>Home</a></Link>
                     <Link href="/jobs"><a>Jobs</a></Link>
                     <Link href="/companies"><a>Companies</a></Link>
